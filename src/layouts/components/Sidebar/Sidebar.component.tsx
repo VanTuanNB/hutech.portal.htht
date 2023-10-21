cb7148c2d2db745953811895e2cccf248df6c66c
phatcana116 <phatcana116@gmail.com>
2023-10-21 23:53:14 +0700

    set up header UI

--- src/layouts/components/Sidebar/Sidebar.component.tsx
+++ src/layouts/components/Sidebar/Sidebar.component.tsx
@@ -4,5 +4,13 @@ import styles from './Sidebar.module.css';
 const cx = classNames.bind(styles);
 
 export default function SidebarComponent() {
-    return <div className={cx('sidebar')}></div>;
+   return <div className={cx('sidebar')}>
+    <div className={cx('sidebar-button')}>
+        <button style={{fontSize: '24px'}}>Sổ giao dịch</button>
+    </div>
+    <div className={cx('sidebar-button')}>
+        <button style={{fontSize: '24px'}}> Biểu đồ</button>
+  </div>
+</div>
+
 }
